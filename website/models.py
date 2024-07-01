19eaac5cf61879517dfc96ef7ab0259d9c189419
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-07-01 17:43:15 -0500

    "created Note and User model using libraries sqlalchemy.sql, func, and usermixin"

---
+++ website/models.py
@@ -0,0 +1,14 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  data = db.Column(db.String(10000))
+  date = db.Column(db.DateTime(timezone=True), default=func.now)#func gets current date and time and stores default value for date
+    
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(150), unique=True)# no user can have the same email (150 character limit)
+  password = db.Column(db.String(150))
+  first_name = db.Column(db.string(150))
