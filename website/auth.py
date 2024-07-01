e09a2ee9854b7e702c60324bdd1993b06e429cd1
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-07-01 15:38:50 -0500

    "add conditionals for user information"

--- website/auth.py
+++ website/auth.py
@@ -18,6 +18,18 @@ def sign_up():
     if request.method == 'POST': 
         email = request.form.get('email')
         firstName = request.form.get('firstName')
-        password = request.form.get('password1')
+        password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        
+        if len(email) < 4: 
+            pass
+        elif len(firstName) < 2: 
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            pass
+            # add user to database
     return render_template("sign_up.html", boolean=True)
