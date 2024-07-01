c4b02dc79fd23ceb642a4cf4331bd0dc243ac7b1
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-07-01 17:50:44 -0500

    "create one (user) to many (notes) relationship with foreign key"

--- website/models.py
+++ website/models.py
@@ -6,9 +6,12 @@ class Note(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   data = db.Column(db.String(10000))
   date = db.Column(db.DateTime(timezone=True), default=func.now)#func gets current date and time and stores default value for date
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  
     
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(150), unique=True)# no user can have the same email (150 character limit)
   password = db.Column(db.String(150))
   first_name = db.Column(db.string(150))
+  notes = db.relationship('Note')
