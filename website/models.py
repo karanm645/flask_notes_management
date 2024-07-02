5f30b6c09bc9a691832b4e9d87038dff3e6d6ef3
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-07-01 23:32:18 -0500

    "missing parenthesis for datetime"

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 class Note(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   data = db.Column(db.String(10000))
-  date = db.Column(db.DateTime(timezone=True), default=func.now)#func gets current date and time and stores default value for date
+  date = db.Column(db.DateTime(timezone=True), default=func.now())#func gets current date and time and stores default value for date
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
   
     
