18593f03a39cece64ae17e7d5dd3c1ea01123f64
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-07-01 22:34:15 -0500

    "add login manager to load user in init.py,  add flask login to logout route in auth.py file, add flask login library to views file"

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -15,6 +16,7 @@ def login():
         if user: 
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category="success")
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash("Incorrect password, try again", category="error")
@@ -23,8 +25,10 @@ def login():
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("login.html", text="testing", user="Karan")
+    logout_user
+    return redirect(url_for('auth.login'))
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
@@ -54,6 +58,7 @@ def sign_up():
             )
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash("Account created!", category="success")
             return redirect(url_for('views.home'))
             # add user to database
