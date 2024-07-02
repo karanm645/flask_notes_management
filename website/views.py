9b3b7922437c93508cb71f35dabff2c3885b1a17
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-07-02 00:27:49 -0500

    "create delete note method"

--- website/views.py
+++ website/views.py
@@ -1,9 +1,34 @@
-from flask import Blueprint, render_template # bp means bunch of routes defined
+from flask import Blueprint, render_template, request, flash, jsonify # bp means bunch of routes defined
 from flask_login import login_required, current_user
+import json
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/') #decorator + route
+@views.route('/', methods=['GET', 'POST']) #decorator + route
 @login_required
 def home(): # will run for root page
+    if request.method == "POST":
+        note = request.form.get("note")  # Gets the note from the HTML
+
+        if len(note) < 1:
+            flash("Note is too short!", category="error")
+        else:
+            new_note = Note(data=note, user_id=current_user.id)  # providing the schema for the note
+            db.session.add(new_note)  # adding the note to the database
+            db.session.commit()
+            flash("Note added!", category="success")
+
     return render_template("home.html", user=current_user)
+
+@views.route("/delete-note", methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
